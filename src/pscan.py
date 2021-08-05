6380e51401a8e05869c770bcf2802ded127cdf30
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-08-05 15:40:25 -0300

    pscan longe de terminado

--- src/pscan.py
+++ src/pscan.py
@@ -6,24 +6,71 @@ import re
 
 def print_hlp():
 
-    s =  '''
-    this app bla bla bla
+    s =  '''usage:
+    pscan --help
+    pscan [ options ] url [ start ] [ end ] [ timeout ]
+    
+    url   -> the url of the target host
+
+    start -> the first port number to be scanned
+             default 1.
+
+    end   -> last to be scanned ( inclusive )
+             default 65535.
+
+    timeout -> the amount of time ( in seconds ) to wait 
+               for target's response before giving the connection up.
+               default = 22, min = 3
+
+    options are:
+
+        s -> if no protocol is known for a given port number
+             do not try to connect
+
     '''
 
     print( s )
 
 def handle_opts( opt ):
-    pass
+    
+    foo = iteractive.iteractive_scan
+
+    must_serv = False
+    if opt == "-s":
+        must_serv = True
+
+    def fun( args ):
+        host_name , start , end , timeout = args
+        return foo( host_name , start , end , timeout, must_serv )
+    return fun
 
 def handle_vals( val_lst ):
     pass
 
-def handle_url( url ):
-
-    regex = r''
-
 def handle_input( entry ):
-    pass
+    
+    option_pat = r'\-[a-z]+(?=\s)'
+    s = re.search( option_pat , entry )
+    opt_str = ""
+    if not( s is None ):
+        opt_str = s.group( 0 )
+    # print( opt_str )
+    # f = handle_opts( opt_str )
+
+    url_pat = r'((\w+\.)+\w+)\b'
+    s = re.search( url_pat , entry )
+    url_str = "localhost"
+    if not( s is None ):
+        url_str = s.groups( 0 )[0]
+    # print( url_str )
+
+    values_pat = r'((\s\d+){,3})$'
+    s = re.search( values_pat , entry )
+    values_str = ""
+    if not( s is None ):
+        values_str = s.groups( 0 )[0]
+    print( values_str )
+
 
 def valid_input( entry ):
     
@@ -44,6 +91,7 @@ def run( f, args ):
 if __name__ == "__main__":
 
     entry = " ".join( sys.argv[ 1: ] )
+    print( entry )
 
     if entry == "--help":
         print_hlp()
